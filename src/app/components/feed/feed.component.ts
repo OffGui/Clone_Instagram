45a26fc1d15f946f02df39ebf611e53b41a2d9ec
Guilherme de Sousa Prata <guilherme.prata@totvs.com.br>
2024-10-25 19:48:10 -0300

    configuração build inicial

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { PostComponent } from '../post/post.component';
 import { ListViewModule } from '@syncfusion/ej2-angular-lists';
+import { Post } from '../../types/post';
 
 @Component({
   selector: 'app-feed',
