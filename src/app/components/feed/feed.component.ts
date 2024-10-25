e3ae82e9deabefbdcd776858300d57c37690a97a
Guilherme de Sousa Prata <guilherme.prata@totvs.com.br>
2024-10-25 19:46:11 -0300

    Criação do componente Feed

---
+++ src/app/components/feed/feed.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { PostComponent } from '../post/post.component';
+import { ListViewModule } from '@syncfusion/ej2-angular-lists';
+
+@Component({
+  selector: 'app-feed',
+  standalone: true,
+  imports: [PostComponent, ListViewModule],
+  templateUrl: './feed.component.html',
+  styleUrl: './feed.component.scss'
+})
+export class FeedComponent {
+  @Input() posts: Post[] = [];
+}
